9213897d559b94893336d2f8ab7e80de419433cb
Sahil Punjabi <sahilpanjab123@gmail.com>
2021-05-23 22:11:17 +0530

    Updates

---
+++ precache-manifest.bcf57c663e342fde6c34a42ca8895011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff8ffc3a9b7b281b0cae1b423fc87e4",
+    "url": "/React-Budget-Calculator/index.html"
+  },
+  {
+    "revision": "304765ae8d87d82ba57d",
+    "url": "/React-Budget-Calculator/static/css/main.c64f5077.chunk.css"
+  },
+  {
+    "revision": "c7ccfa1326dcd7e1a514",
+    "url": "/React-Budget-Calculator/static/js/2.cd00c108.chunk.js"
+  },
+  {
+    "revision": "304765ae8d87d82ba57d",
+    "url": "/React-Budget-Calculator/static/js/main.6c0738dc.chunk.js"
+  },
+  {
+    "revision": "2aeb3b3086fe7865e904",
+    "url": "/React-Budget-Calculator/static/js/runtime~main.26d29c98.js"
+  }
+]);
